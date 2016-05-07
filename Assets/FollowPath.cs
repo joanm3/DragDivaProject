f8b07f6fc9fb4c52f9f242ee5e5092c2b6722510
joanm3 <joanm3@hotmail.com>
2016-05-07 16:35:15 +0200

    shaders tutorial. https://cgcookie.com/archive/noob-to-pro-shader-writing-for-unity-4-beginner/

--- Assets/FollowPath.cs
+++ Assets/FollowPath.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class FollowPath : MonoBehaviour {
 
+    //https://cgcookie.com/archive/noob-to-pro-shader-writing-for-unity-4-beginner/ shaders unity
+
     [SerializeField]
     private Paths path;
     [SerializeField]
@@ -20,6 +22,8 @@ public class FollowPath : MonoBehaviour {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
         if (distanceFromWayPoint < distanceToChange)
         {
+
+
             if (currentWaypoint >= path.getLength() - 1)
                 currentWaypoint = 0;
             else
